8cc4ed3f6f1e58e2ac97d382dfb66a553799d5f1
TTLAB <ttlab@TTLABs-MacBook-Pro.local>
2026-06-29 01:09:10 +0700

    chore: config commit lint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,36 @@
-export default { extends: ['@commitlint/config-conventional'] };
+// export default { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  parserPreset: {
+    parserOpts: {
+      headerPattern: /^(\w*)(?:\((.*)\))?: (.*)$/,
+      headerCorrespondence: ['type', 'scope', 'subject'],
+    },
+  },
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'chore',
+        'revert',
+        'build',
+        'ci',
+      ],
+    ],
+    'header-min-length': [2, 'always', 10],
+    'header-max-length': [2, 'always', 100],
+    'subject-case': [
+      0,
+      'never',
+      ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
+    ],
+  },
+};
