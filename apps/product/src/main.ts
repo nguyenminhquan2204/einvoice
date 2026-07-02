1d75c9ccca1582cf46907d5a93139b649af886f8
TTLAB <ttlab@TTLABs-MacBook-Pro.local>
2026-07-02 17:11:08 +0700

    feat: product service, create new container postgresql - pgadmin

---
+++ apps/product/src/main.ts
@@ -0,0 +1,30 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { AppModule } from './app/app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.TCP,
+    options: {
+      host: AppModule.CONFIGURATION.TCP_SERV.TCP_INVOICE_SERVICE.options?.host,
+      port: AppModule.CONFIGURATION.TCP_SERV.TCP_INVOICE_SERVICE.options?.port,
+    },
+  });
+
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+  const port = process.env.PRODUCT_PORT || 3000;
+
+  await app.startAllMicroservices();
+  await app.listen(port);
+  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+}
+
+bootstrap();
